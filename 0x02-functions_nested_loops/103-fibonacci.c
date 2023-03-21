2fcb60150608ef2586654128bbd5c81b86caca09
ramy nageh <ramy-nageh@live.com>
2023-03-21 15:53:04 -0700

    all

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,21 +6,21 @@
 */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
-	j = 1;
-	k = 2;
-	sum = 0;
-	for (i = 1; i <= 33; ++i)
+int i;
+unsigned long int j, k, next, sum;
+j = 1;
+k = 2;
+sum = 0;
+for (i = 1; i <= 33; ++i)
+{
+	if (j < 4000000 && (j % 2) == 0)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		sum = sum + j;
 	}
-	printf("%lu\n", sum);
-	return (0);
+	next = j + k;
+	j = k;
+	k = next;
+}
+printf("%lu\n", sum);
+return (0);
 }
